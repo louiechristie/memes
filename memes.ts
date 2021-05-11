86277567165d799289e4c1bebdeb9fe0cc8ea66e
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2021-05-11 13:01:28 +0100

    meme: nanoseconds

--- memes.ts
+++ memes.ts
@@ -55,11 +55,21 @@ export const getLongTitle = (meme: Meme) => {
 
 const memes: Meme[] = [
   {
+    url: "nanoseconds",
+    title: "Nanoseconds",
+    image: "https://img.youtube.com/vi/9eyFDBPk4Yw/0.jpg",
+    alt: "Grace hopper on chat show Letterman (late night TV old footage) holds a peice of wire that demonstrates the maximum distance an electron can travel in a nanosecond.",
+    youtube: {
+      v: "lGTEUtS5H7I",
+      start: 1 * 60 + 59,
+      end: 2 * 60 + 26,
+    },
+  },
+  {
     url: "this-lifestyle-is-inhumane",
     title: "This lifestyle is inhumane",
     image: "http://www.sinz.org/Michael.Sinz/Art/Evolution.jpg",
-    alt:
-      "Pictures of the evolution of a human man, in a historical painted style, from hunched ape to standing naked hunter, back to man hunched over a computer naked.",
+    alt: "Pictures of the evolution of a human man, in a historical painted style, from hunched ape to standing naked hunter, back to man hunched over a computer naked.",
     youtube: {
       v: "agOdP2Bmieg",
       start: 13 * 60 + 5,
@@ -69,19 +79,15 @@ const memes: Meme[] = [
       "‟this style of knowledge work, this lifestyle, is inhumane” - Bret Victor (ex-Apple inventor)",
     footnotes: [
       {
-        text:
-          "‟Something, somewhere went terribly wrong” image by an unknown artist (Localoaf, 2011) cited researchgate.net 2021",
-        link:
-          "https://www.researchgate.net/figure/Something-somewhere-went-terribly-wrong-image-by-an-unknown-artist-Localoaf-2011_fig3_267398000",
+        text: "‟Something, somewhere went terribly wrong” image by an unknown artist (Localoaf, 2011) cited researchgate.net 2021",
+        link: "https://www.researchgate.net/figure/Something-somewhere-went-terribly-wrong-image-by-an-unknown-artist-Localoaf-2011_fig3_267398000",
       },
       {
-        text:
-          "Full 1 hour talk video: The Humane Representation of Thought by Bret Victor",
+        text: "Full 1 hour talk video: The Humane Representation of Thought by Bret Victor",
         link: "https://www.youtube.com/watch?v=agOdP2Bmieg",
       },
       {
-        text:
-          "(He's an ex-Apple Human-Interface inventor in the Future-Interface Team)",
+        text: "(He's an ex-Apple Human-Interface inventor in the Future-Interface Team)",
         link: "http://worrydream.com/#!/Apple",
       },
     ],
@@ -90,8 +96,7 @@ const memes: Meme[] = [
     url: "squeezed-apple",
     title: "Squeezed Apple",
     image: "https://img.youtube.com/vi/1s1uWo1_bzg/maxresdefault.jpg",
-    alt:
-      "Tim Cook being asked an uncomfortable question in a political house hearing about why developers can only sell apps throug the app store. Tim Looks like he's squeezing out a tough turd.",
+    alt: "Tim Cook being asked an uncomfortable question in a political house hearing about why developers can only sell apps throug the app store. Tim Looks like he's squeezing out a tough turd.",
     youtube: {
       v: "1s1uWo1_bzg",
       start: 1 * 60 * 60 + 14 * 60 + 6,
@@ -101,8 +106,7 @@ const memes: Meme[] = [
     footnotes: [
       {
         text: "Beavis",
-        link:
-          "https://en.wikipedia.org/wiki/File:Beavis_BeavisandButtHead.png#/media/File:Beavis_BeavisandButtHead.png",
+        link: "https://en.wikipedia.org/wiki/File:Beavis_BeavisandButtHead.png#/media/File:Beavis_BeavisandButtHead.png",
       },
     ],
   },
@@ -111,8 +115,7 @@ const memes: Meme[] = [
     url: "good-news",
     title: "The world is actually getting better",
     image: "https://img.youtube.com/vi/jbkSRLYSojo/hq2.jpg",
-    alt:
-      "Animated graph showing the world is actually getting richer and healthier since presented by Hans Rosling.",
+    alt: "Animated graph showing the world is actually getting richer and healthier since presented by Hans Rosling.",
     youtube: {
       v: "jbkSRLYSojo",
       start: 2 * 60 + 49,
@@ -121,15 +124,12 @@ const memes: Meme[] = [
     caption: "Shame bad news sells",
     alsoSee: [
       {
-        text:
-          "Bill Gate's Favourite book of all time is Steven Pinker's 'Enlightenment Now'. There's a reason why. [3 min video]",
+        text: "Bill Gate's Favourite book of all time is Steven Pinker's 'Enlightenment Now'. There's a reason why. [3 min video]",
         link: "https://www.youtube.com/watch?v=ABVVDSgihYs",
       },
       {
-        text:
-          "Hans Rosling full talk on the best stats you've never seen [20 min video]",
-        link:
-          "https://www.ted.com/talks/hans_rosling_the_best_stats_you_ve_ever_seen#t-215279",
+        text: "Hans Rosling full talk on the best stats you've never seen [20 min video]",
+        link: "https://www.ted.com/talks/hans_rosling_the_best_stats_you_ve_ever_seen#t-215279",
       },
     ],
     footnotes: [
@@ -144,8 +144,7 @@ const memes: Meme[] = [
     url: "dissociation",
     title: "How I feel when filling out questionnaires",
     image: "https://img.youtube.com/vi/OqSg7WO4tT4/hq1.jpg",
-    alt:
-      "Tom Hanks in Saving Private Ryan - storming the beach in during D-Day spinning out in the chaos and stress.",
+    alt: "Tom Hanks in Saving Private Ryan - storming the beach in during D-Day spinning out in the chaos and stress.",
     youtube: {
       v: "OqSg7WO4tT4",
       start: 0,
@@ -153,8 +152,7 @@ const memes: Meme[] = [
     },
     footnotes: [
       {
-        text:
-          "Video: Tom Hanks in Saving Private Ryan (1998) storming the beach during D-Day (Normandy in the second World War retaking France from the Nazis.) He's trying to anyway.",
+        text: "Video: Tom Hanks in Saving Private Ryan (1998) storming the beach during D-Day (Normandy in the second World War retaking France from the Nazis.) He's trying to anyway.",
         link: "https://www.imdb.com/title/tt0120815/",
       },
     ],
@@ -163,8 +161,7 @@ const memes: Meme[] = [
     url: "banter-is-classless",
     title: "Banter is classless",
     image: "https://img.youtube.com/vi/7QINKUsxiHA/hq2.jpg",
-    alt:
-      "An upper class woman from Chelsea has a date with a working class man from London. They both enjoy each other's 'banter'.",
+    alt: "An upper class woman from Chelsea has a date with a working class man from London. They both enjoy each other's 'banter'.",
     youtube: {
       v: "7QINKUsxiHA",
       start: 1 * 60 + 58,
@@ -176,8 +173,7 @@ const memes: Meme[] = [
     url: "how-many-computer-geeks-to-change-a-light-bulb",
     title: "How many computer geeks does it take to change a light bulb?",
     image: "https://i.imgflip.com/4reggs.jpg",
-    alt:
-      "Light bulb. Title reads: How many computer geeks does it take to change a light bulb? One, but I take days connecting it to the internet to save time flicking the switch. louiechristie.com © 2020",
+    alt: "Light bulb. Title reads: How many computer geeks does it take to change a light bulb? One, but I take days connecting it to the internet to save time flicking the switch. louiechristie.com © 2020",
     caption:
       "... one, but I take days connecting it to the internet to save time flicking the switch 💡",
   },
@@ -185,8 +181,7 @@ const memes: Meme[] = [
     url: "home-working-space-saver-bed",
     title: "Home working space saver bed",
     image: "https://img.youtube.com/vi/UOHI8qdZkH8/hq1.jpg",
-    alt:
-      "Muppets sat on a murphy space saver hideaway bed. It folds up with them in it.",
+    alt: "Muppets sat on a murphy space saver hideaway bed. It folds up with them in it.",
     youtube: {
       v: "UOHI8qdZkH8",
       start: 2 * 60 + 2,
@@ -208,8 +203,7 @@ const memes: Meme[] = [
     url: "finland-happy",
     title: "How the Finnish are world happiest",
     image: "https://img.youtube.com/vi/9FPU4F-Ajh8/0.jpg",
-    alt:
-      "Woman in hat and winter clothes (looks like skiing type clothes) sits at a table in the snow, she looks unhappy.",
+    alt: "Woman in hat and winter clothes (looks like skiing type clothes) sits at a table in the snow, she looks unhappy.",
     caption:
       "\"When you walk on the streets in Finland, people don't seem to be particularly happy, and I would actually think that might contribute positively to our sense of satisfaction... if you live among people where everyone's kind of like advertising their own happiness, like telling 'hey, look at me how happy I am' that might actually make you like more sad.\" - Frank Martela PhD Psychology, Aalto University",
     youtube: {
@@ -222,8 +216,7 @@ const memes: Meme[] = [
     url: "face-masks",
     title: "If you feel that face masks don't protect you...",
     image: "https://img.youtube.com/vi/8IjXzadiNaA/0.jpg",
-    alt:
-      "World map with red dots all over it. USA is particularly covered in red dots. Title over the map in big letters reads 'THIS WEEK IN VIROLOGY #673 WAKE UP AND SMELL THE PANDEMIC'",
+    alt: "World map with red dots all over it. USA is particularly covered in red dots. Title over the map in big letters reads 'THIS WEEK IN VIROLOGY #673 WAKE UP AND SMELL THE PANDEMIC'",
     caption:
       "‘... then the next time you have surgery tell your surgical team not to wear face masks’ - Vincent Racaniello, Professor of Immunology at Columbia University",
     youtube: {
@@ -233,10 +226,8 @@ const memes: Meme[] = [
     },
     footnotes: [
       {
-        text:
-          "As I was making this (during the 2020 pandemic) some fucking muppets interrupted selling door-to-door, one with a mask that didn't cover her nose. 😡",
-        link:
-          "https://twitter.com/louiechristie/status/1223012411172622340?s=20",
+        text: "As I was making this (during the 2020 pandemic) some fucking muppets interrupted selling door-to-door, one with a mask that didn't cover her nose. 😡",
+        link: "https://twitter.com/louiechristie/status/1223012411172622340?s=20",
       },
     ],
   },
@@ -244,8 +235,7 @@ const memes: Meme[] = [
     url: "wasting-your-life",
     title: "Wasting Your Life",
     image: "https://img.youtube.com/vi/TVlIbE0dSgQ/hq1.jpg",
-    alt:
-      "Alan Partridge taking a phone call while sitting on bed in a hotel bedroom. On the chest of draws in front of him is a portrait of David Hasselhoff",
+    alt: "Alan Partridge taking a phone call while sitting on bed in a hotel bedroom. On the chest of draws in front of him is a portrait of David Hasselhoff",
     caption: "Alan Partridge talks to his son Fernando.",
     youtube: {
       v: "TVlIbE0dSgQ",
@@ -257,8 +247,7 @@ const memes: Meme[] = [
     url: "valuable-lesson",
     title: "Valuable Lesson",
     image: "https://i.imgflip.com/447g00.jpg",
-    alt:
-      "Bearded man (Louie Christie) in a bubble bath smirking. Text reads: 'I learned a valuable lesson today... Don’t eat baked beans, before a bath'",
+    alt: "Bearded man (Louie Christie) in a bubble bath smirking. Text reads: 'I learned a valuable lesson today... Don’t eat baked beans, before a bath'",
     caption:
       "I learned a valuable lesson today... Don’t eat baked beans, before a bath. 🛁 😬",
   },
@@ -266,11 +255,9 @@ const memes: Meme[] = [
     url: "bob-monkhouses-joke-book",
     title: "Bob Monkhouse's Joke Book",
     image: "https://ichef.bbci.co.uk/images/ic/640x360/p08mm605.jpg",
-    alt:
-      "Black and white photo of young Bob Monkhouse. Bob is holding a phone and grinning inanely",
+    alt: "Black and white photo of young Bob Monkhouse. Bob is holding a phone and grinning inanely",
     bbc: {
-      link:
-        "https://www.bbc.co.uk/iplayer/episode/b00x9b7w/the-secret-life-of-bob-monkhouse#t=49m27s,",
+      link: "https://www.bbc.co.uk/iplayer/episode/b00x9b7w/the-secret-life-of-bob-monkhouse#t=49m27s,",
     },
   },
   {
@@ -288,8 +275,7 @@ const memes: Meme[] = [
     url: "resusci-annie-the-musical",
     title: "Resusci Annie The Musical",
     image: "https://img.youtube.com/vi/AZskwWOr5xs/hq3.jpg",
-    alt:
-      "A woman and a man on a stage. A title overlay says 'Anne is the most kissed face of all time'",
+    alt: "A woman and a man on a stage. A title overlay says 'Anne is the most kissed face of all time'",
     youtube: {
       v: "AZskwWOr5xs",
       start: 444,
@@ -301,8 +287,7 @@ const memes: Meme[] = [
     title: "Time Management",
     image:
       "https://www.louiechristie.com/images/time-management-cube-by-louie-christie-2018_me7fst_c_scale,w_1307.jpg",
-    alt:
-      "A cube hessian storage box with post-it notes in corners. One says 'WIN WIN'",
+    alt: "A cube hessian storage box with post-it notes in corners. One says 'WIN WIN'",
     customHTML: true,
   },
   {
@@ -346,16 +331,14 @@ const memes: Meme[] = [
     title: "Remote Working ",
     image:
       "https://embedwistia-a.akamaihd.net/deliveries/d4e3c25ccb18b0bfba2caca8ba4f1b4c1cb5a037.jpg",
-    alt:
-      "A man presenting a news type TV show with two guests on video link. Title reads 'TECH TALK - REMOTE WORKING'",
+    alt: "A man presenting a news type TV show with two guests on video link. Title reads 'TECH TALK - REMOTE WORKING'",
     customHTML: true,
   },
   {
     url: "😬",
     title: "😬",
     image: "https://img.youtube.com/vi/nbr1VUlH4Mg/hqdefault.jpg",
-    alt:
-      "Charlie Chaplain and a woman in a a bonnet walking down a long straight dusty country road.",
+    alt: "Charlie Chaplain and a woman in a a bonnet walking down a long straight dusty country road.",
     youtube: {
       v: "nbr1VUlH4Mg",
       start: 94,
@@ -371,8 +354,7 @@ const memes: Meme[] = [
     image: "https://img.youtube.com/vi/XQzQRaQINns/hqdefault.jpg",
     width: 500,
     height: 500,
-    alt:
-      "Joey from Friends TV show in a classroom sat casually on a desk at the front of a blackboard. Students are watching.",
+    alt: "Joey from Friends TV show in a classroom sat casually on a desk at the front of a blackboard. Students are watching.",
     youtube: {
       v: "XQzQRaQINns",
       start: 67,
@@ -383,8 +365,7 @@ const memes: Meme[] = [
     url: "rules",
     title: "Rules",
     image: "https://img.youtube.com/vi/fCjsUxbNmIs/hqdefault.jpg",
-    alt:
-      "Back to the future Delorean car with characters played by Michael J. Fox, Elisabeth Shue and Christopher Lloyd sat in the front. Christopher Lloyd is dressed as mad professor 'Doc' Brown and is wearing a futuristic silver sunglasses visor.",
+    alt: "Back to the future Delorean car with characters played by Michael J. Fox, Elisabeth Shue and Christopher Lloyd sat in the front. Christopher Lloyd is dressed as mad professor 'Doc' Brown and is wearing a futuristic silver sunglasses visor.",
     youtube: {
       v: "fCjsUxbNmIs",
       start: 110,
@@ -395,8 +376,7 @@ const memes: Meme[] = [
     url: "flowers",
     title: "Flowers",
     image: "https://img.youtube.com/vi/yKhOYvDRKrg/hqdefault.jpg",
-    alt:
-      "A buddhist monk in brown robes with a shaved head sits cross legged in front of flowers, and greenery",
+    alt: "A buddhist monk in brown robes with a shaved head sits cross legged in front of flowers, and greenery",
     youtube: {
       v: "yKhOYvDRKrg",
       start: 36,
