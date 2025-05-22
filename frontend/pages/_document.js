f5d5391590f6a2ab705263d26cbd7250e7d99f47
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-05-22 22:39:40 +0100

    refactor nav css to use external

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="stylesheet" href="https://www.louiechristie.com/stylesheets/nav.css" />
           <link
             href="https://fonts.googleapis.com/css?family=Roboto+Mono:700|Roboto:400,900&display=swap"
             rel="stylesheet"
