747f21c0c1195ceed8e886833f1a5d1139cb0e84
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-05-22 21:56:38 +0100

    remove outdated code privacy statement

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -4,12 +4,6 @@ export default function Index() {
       <p>
         Made by <a href="/">Louie Christie</a>
       </p>
-
-      <p>
-        <a href="https://help.github.com/articles/github-privacy-statement/#information-from-website-browsers">
-          privacy statement
-        </a>
-      </p>
     </footer>
   );
 }
