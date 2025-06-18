6b8f1c9df4222173ba56d3d2cfbedea227c803d6
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-06-18 13:48:35 +0100

    Spell check memes
    :

--- frontend/memes.ts
+++ frontend/memes.ts
@@ -151,7 +151,7 @@ const memes: Meme[] = [
     image: "http://img.youtube.com/vi/ecKgqJRvZ5M/2.jpg",
     width: 1280,
     height: 720,
-    alt: "Bald guy sat at a microphone in front of a wooden table in techy office with men's toys on shelves (tech mostly). He's sat in a video gaming chair.  In a room that is otherwise sparse and spacious with wooden floors",
+    alt: "Bald guy sat at a microphone in front of a wooden table in techie office with men's toys on shelves (tech mostly). He's sat in a video gaming chair.  In a room that is otherwise sparse and spacious with wooden floors",
     caption: `"He could go out and hire, because he's Apple, the best software coder in the world - but he'd rather hire a great software coder who also did stand-up comedy."  - Apple exec Andy Miller on Steve Jobs`,
     youtube: {
       v: "ecKgqJRvZ5M",
